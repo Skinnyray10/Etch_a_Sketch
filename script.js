6a0bbff3c3767feba7a70dd67c2fe8de24f0436f
Skinnyray10 <ryangarciagalvan10@gmail.com>
2026-06-08 22:05:24 -0500

    For loop done and the css is now working properly, the container and square are done

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const container = document.getElementById("container");
-const square = 
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    container.appendChild(square)
+}
+
