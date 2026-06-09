c512635afb5030cb2f1949533ec51a33cb59fa00
Skinnyray10 <ryangarciagalvan10@gmail.com>
2026-06-08 23:37:37 -0500

    now the grid its done and the button is working perfectly

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const container = document.getElementById("container");
+const buttonNewGrid = document.getElementById("newgrid");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div")
-    square.classList.add("square")
-    container.appendChild(square)
+
+
+function createGrid(size) {
+    container.innerHTML = ""
+    for (let i = 0; i < size * size; i++) {
+
+        const square = document.createElement("div")
+        square.classList.add("square")
+        container.appendChild(square)
+        square.addEventListener('mouseover', () =>
+            square.style.backgroundColor = "red"
+        )
+    }
+
 }
+buttonNewGrid.addEventListener("click", () => {
+    let size = prompt("Tell us the size of the grid");
+
+    let numero = parseInt(size, 10)
+
+    if (isNaN(numero)) {
+        numero = 16
+    }
+    else if (numero > 100) {
+        numero = 100
+    }
+    createGrid(numero);
+});
+
 
